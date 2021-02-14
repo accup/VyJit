3bdbc5c2987a33102424937ef4de169a56c7b81d
Accup <32096121+accup@users.noreply.github.com>
2021-02-14 13:44:28 +0900

    [add, move] 解析器のテンプレート位置を変更し、不明な名前に対してNotFoundを返すよう修正

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from aiohttp import web
 from aiohttp_jinja2 import render_template
+import analyzers
 
 
 routes = web.RouteTableDef()
@@ -14,13 +15,18 @@ async def index(request: web.Request):
     )
 
 
-@routes.get('/analyzer/{analysis_name}')
+@routes.get('/analyzers/{analyzer_name}')
 async def analysis(request: web.Request):
-    analysis_name = request.match_info['analysis_name']
+    analyzer_name = request.match_info['analyzer_name']
+    if analyzer_name not in analyzers.SUBMODULES:
+        raise web.HTTPNotFound()
+
     return render_template(
-        'pages/{}.html'.format(analysis_name),
+        'analyzers/{}.html'.format(analyzer_name),
         request=request,
-        context=None,
+        context={
+            'analyzer_name': analyzer_name
+        },
     )
 
 
