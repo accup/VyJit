0b4193890c219b413dc35ffd4912ef76260cc686
Accup <32096121+accup@users.noreply.github.com>
2021-02-22 20:30:41 +0900

    [move, add] VSCodeにおけるグローバルオブジェクトの補完のためのファイルを追加

--- global.d.ts
+++
@@ -1,2 +0,0 @@
-declare var analyzer: typeof import('./js/analyzer');
-declare var r6r: typeof import('./js/canvas-renderer')
---
+++ globals.d.ts
@@ -0,0 +1,2 @@
+declare var analyzer: typeof import('./js/analyzer');
+declare var r6r: typeof import('./js/canvas-renderer');
