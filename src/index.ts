feed1ca200b917cd26775bff3e3a98f2eac6db2c
Accup <32096121+accup@users.noreply.github.com>
2021-02-22 22:59:55 +0900

    [update] r6rが見えなくなっていたのを修正

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 require('expose-loader?exposes=io!socket.io-client');
 require('expose-loader?exposes=d3!d3');
 require('expose-loader?exposes=analyzer|default!./analyzer');
-require('expose-loader?exposes=r6r|default!./canvas-renderer');
+require('expose-loader?exposes=r6r!./canvas-renderer');
